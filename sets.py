144b19d9c988a8809ed278e49139265b0f7e57e8
Victoria De Santis <162721946+vicdsantis@users.noreply.github.com>
2024-05-26 22:50:35 -0300

    entrega sets.py

--- sets.py
+++ sets.py
@@ -2,30 +2,15 @@ from sets_categories_data import (ALCOHOLS)
 
 
 def clean_ingredients(dish_name, dish_ingredients):
-    """Remove duplicates from `dish_ingredients`.
-
-    :param dish_name: str - containing the dish name.
-    :param dish_ingredients: list - dish ingredients.
-    :return: tuple - containing (dish_name, ingredient set).
-
-    This function should return a `tuple` with the name of the dish as the first item,
-    followed by the de-duped `set` of ingredients as the second item.
-    """
-
-    return ()
+    dish_ing1 = set(dish_ingredients)
+    return(dish_name, dish_ing1)
 
 
 def check_drinks(drink_name, drink_ingredients):
-    """Append "Cocktail" (alcohol)  or "Mocktail" (no alcohol) to `drink_name`, based on `drink_ingredients`.
+    drin_ing2 = set(drink_ingredients)
+    alcohol_1 = set(ALCOHOLS)
 
-    :param drink_name: str - name of the drink.
-    :param drink_ingredients: list - ingredients in the drink.
-    :return: str - drink_name appended with "Mocktail" or "Cocktail".
-
-    The function should return the name of the drink followed by "Mocktail" (non-alcoholic) and drink
-    name followed by "Cocktail" (includes alcohol).
-
-    """
-
-    return ""
-
+    if drin_ing2.intersection(alcohol_1) == set():
+        return (str(drink_name) + " " + "Mocktail")
+    else:
+        return (str(drink_name) + " " + "Cocktail")
